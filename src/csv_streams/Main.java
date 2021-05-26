76e4d06c29f3df15a329f426b405172663ece9d2
ahmedmansour <a.a2051995@gmail.com>
2021-05-27 00:45:01 +0200

    add the hightest population capital and hightest pop city in every country to main to test them

--- src/csv_streams/Main.java
+++ src/csv_streams/Main.java
@@ -3,6 +3,9 @@ package csv_streams;
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -19,12 +22,26 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		Map<String, List<City>> country_cities = countryDAO.build_country_Map(cities);
-//		for(String key : country_cities.keySet()) {
-//			for(City city: country_cities.get(key)) {
-//				System.out.println("country code:"+key+"  "+city.getName());
-//			}
-//		}
+		
+		countryDAO.sort_cities(country_cities, "ZAF");
+		for(String key : country_cities.keySet()) {
+			for(City city: country_cities.get(key)) {
+				//if("ZAF".equals(key))
+				//	System.out.println("country code:"+key+"  "+city.getPopulation());
+			}
+		}
+		
+		Map<String, City> country_city_high_pop = CountryStreams.hightest_pop_City(country_cities);
+		for(String key:country_city_high_pop.keySet()) {
+			System.out.println("city name"+country_city_high_pop.get(key).getName()+
+					" "+country_city_high_pop.get(key).getPopulation());
+		}
+		City hightcpital_pop = CountryStreams.hightest_pop_capital(countries, cities); 
+		System.out.println("the hightest pop capital is"+hightcpital_pop); 
+		
+		  
       
 	}
 
